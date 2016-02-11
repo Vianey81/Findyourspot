ce1bbb6d206d96b1d2b321ee101c0e39326ef8ad
Vianey M <vianeymunozg@hotmail.com>
2016-02-10 23:56:29 -0800

    Maps working D3

--- ranking.py
+++ ranking.py
@@ -11,8 +11,42 @@ from model import connect_to_db, db
 from server import app
 
 
+def get_top_counties(tax, profession, marital, wl, wp, wc, wm):
+    """ Rank states and return a dictionary with all states"""
+
+    query = """select co.state_id, co.county_id, co.name, sl.rank as livingrank,
+               (cp.a_mean-cl.tax_"""+tax+""")as professionrank, cc.total as crimerank,
+               (cm.male_"""+marital+""" + cm.female_"""+marital+""") as totalmarital
+               from counties co
+               left join countiesliving cl on (co.county_id = cl.county_id)
+               left join statesliving sl on (co.state_id = sl.state_id)
+               left join statesprofessions cp on (co.state_id = cp.state_id)
+               left join statescrime cc on (co.state_id = cc.state_id)
+               left join countiesmarital cm on (co.county_id =  cm.county_id)
+               where cp.title = '"""+profession+"""'
+               order by co.county_id"""
+
+    result = db.session.execute(query)
+    df = DataFrame(result.fetchall())
+    df.columns = result.keys()
+
+    df['LivRank'] = (df['livingrank'].rank(ascending=False)) * wl
+    df['profRank'] = df['professionrank'].rank(ascending=True) * wp
+    df['criRank'] = df['crimerank'].rank(ascending=True) * wc
+    df['marRank'] = df['totalmarital'].rank(ascending=True) * wm
+    df['Total'] = (df['LivRank']) + (df['profRank']) + (df['criRank']) + (df['marRank'])
+    df['rankTotal'] = df['Total'].rank(ascending=True)
+
+    rank_counties = df.sort_index(by=['rankTotal'], ascending=False).fillna(0).to_dict('records')
+    create_tsv(df.fillna(0), ["county_id", "rankTotal"])
+    print "Living:", wl, " Prof: ", wp, " Crime: ", wc, " Marital: ", wm
+    return rank_counties
+
+
 def get_top_states(tax, profession, marital, wl, wp, wc, wm):
-    query = """select s.state_id, s.name, l.rank as livingrank,
+    """ Rank states and return a dictionary with all states"""
+
+    query = """select s.state_id as id, s.name, l.rank as livingrank,
                (p.a_mean-l.tax_"""+tax+""")as professionrank, c.total as crimerank,
                (m.male_"""+marital+""" + m.female_"""+marital+""") as totalmarital
                from states s
@@ -31,11 +65,20 @@ def get_top_states(tax, profession, marital, wl, wp, wc, wm):
     df['profRank'] = df['professionrank'].rank(ascending=True) * wp
     df['criRank'] = df['crimerank'].rank(ascending=True) * wc
     df['marRank'] = df['totalmarital'].rank(ascending=True) * wm
-    df['rankTotal'] = (df['LivRank']) + (df['profRank']) + (df['criRank']) + (df['marRank'])
+    df['Total'] = (df['LivRank']) + (df['profRank']) + (df['criRank']) + (df['marRank'])
+    df['rate'] = df['Total'].rank(ascending=True)
 
-    rank_states = df.sort_index(by=['rankTotal'], ascending=False).fillna(0).to_dict('records')
-    print "Living:",wl, " Prof: ", wp, " Crime: ", wc," Marital: ", wm
+    rank_states = df.sort_index(by=['rate'], ascending=False).fillna(0).to_dict('records')
+    create_tsv(df.fillna(0), ["id", "rate"])
+    print "Living:", wl, " Prof: ", wp, " Crime: ", wc, " Marital: ", wm
     return rank_states
+
+
+def create_tsv(df, header):
+    """ Create file tsv to with the results"""
+
+    df.to_csv('data/rankresults.tsv', mode='a', sep='\t', index=False,  columns=header)
+    return None
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, send_from_directory
 from flask_debugtoolbar import DebugToolbarExtension
 
 from sqlalchemy import func
@@ -11,6 +11,9 @@ from model import State, County, StatePopulation, CountyPopulation
 from model import StateProfession, StateLiving, CountyLiving, StateMarital
 from model import CountyMarital, StateCrime, CountyCrime
 import ranking
+from flask import jsonify
+import json
+
 
 app = Flask(__name__)
 
@@ -31,8 +34,8 @@ def index():
     return render_template("homepage.html", professions=professions)
 
 
-@app.route('/search')
-def search():
+@app.route('/search_states')
+def search_states():
     """Search the top states"""
 
     tax = request.args.get("tax")
@@ -46,19 +49,46 @@ def search():
     wm = int(request.args.get("opcMarital"))
     top_states = ranking.get_top_states(tax, profession, mstatus, wl, wp, wc, wm)
 
-    return render_template("rank.html", topstates=top_states)
+    return render_template("rank.html", result=top_states)
 
 
-@app.route("/map")
-def map():
-    """Show the info of a user"""  
-    return render_template("indexd3.html")
+@app.route('/search_counties')
+def search_counties():
+    """Search the top counties"""
+
+    tax = request.args.get("tax")
+    profession = request.args.get("profession")
+    age = request.args.get("age")
+    marital = request.args.get("marital")
+    mstatus = marital + "_" + age
+    wl = int(request.args.get("opcLiving"))
+    wp = int(request.args.get("opcProfession"))
+    wc = int(request.args.get("opcCrime"))
+    wm = int(request.args.get("opcMarital"))
+    top_counties = ranking.get_top_counties(tax, profession, mstatus, wl, wp, wc, wm)
+    return render_template("rank.html", result=top_counties)
 
 
-@app.route("/movie_info/<int:movie_id>")
-def movie_info(movie_id):
-    """Show the info of a particular movie"""
-    pass
+@app.route('/map_counties')
+def map_counties():
+    """Show the map by counties"""
+
+    return render_template("counties.html")
+
+
+@app.route('/data/<path:path>')
+def send_js(path):
+    """Return an specific file from data directory."""
+
+    return send_from_directory('data', path)
+
+
+@app.route('/map_states')
+def map_states():
+    """Show the map by States"""
+
+    return render_template("states.html")
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
